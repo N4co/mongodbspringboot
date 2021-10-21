23479a8ace5d1c0ebeca7891ee073241d5754c88
n4co <nf.oliveira@hotmail.com>
2021-10-21 03:57:15 -0300

    Deleção de usuario com delete

--- src/main/java/com/nicolasfelix/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nicolasfelix/workshopmongo/resources/UserResource.java
@@ -45,8 +45,13 @@ public class UserResource {
 		obj = service.Insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
-
 }
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+
+	}
 }	
 	
 	
