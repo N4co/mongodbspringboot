76913f8b7ec1c19450cc74163cea252ca45a72dc
n4co <nf.oliveira@hotmail.com>
2021-10-21 19:18:43 -0300

    Referenciando os post do usuario

--- src/main/java/com/nicolasfelix/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nicolasfelix/workshopmongo/config/Instantiation.java
@@ -40,10 +40,11 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("22/05/2021"), "Partiu Viagem", "Vou viajar para Ubatuba", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("22/05/2021"), "cheguei Ubta", "Salve a Natureza", new AuthorDTO (maria));
-		
-		
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 		
 	}
 
--- src/main/java/com/nicolasfelix/workshopmongo/domain/User.java
+++ src/main/java/com/nicolasfelix/workshopmongo/domain/User.java
@@ -1,8 +1,12 @@
 package com.nicolasfelix.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
@@ -14,6 +18,8 @@ public class User implements Serializable {
 	private String name;
 	private String email;
 	
+    @DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User () {
 	}
@@ -40,6 +46,27 @@ public class User implements Serializable {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	public List<Post> getPosts() {
+		return posts;
+	}
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
 	}
   
    
