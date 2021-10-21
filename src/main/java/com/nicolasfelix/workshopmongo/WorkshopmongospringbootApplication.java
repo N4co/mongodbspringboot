9120e98faa741cd859b78f1b30e233bac4e2f123
n4co <nf.oliveira@hotmail.com>
2021-10-21 00:52:00 -0300

    Project Create

---
+++ src/main/java/com/nicolasfelix/workshopmongo/WorkshopmongospringbootApplication.java
@@ -0,0 +1,13 @@
+package com.nicolasfelix.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongospringbootApplication.class, args);
+	}
+
+}
